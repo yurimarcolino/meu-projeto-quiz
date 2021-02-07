c800865971708dc636fd4b7092fe363bf4d1bc63
yurimarcolino <yurifmarcolino@gmail.com>
2021-02-07 15:52:17 -0300

    configured project

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // Utiliza aspas simples
+  singleQuote: true, 
+
+  // Insere uma vírgula ao final, sempre que possível
+  trailingComma: 'all',
+
+  // Omite os parênteses ao redor do parâmetro de uma arrow function, sempre que possível 
+  arrowParens: 'avoid',
+}
